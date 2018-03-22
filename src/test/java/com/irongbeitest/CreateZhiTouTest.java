821b62b3d6aa96ed53dc780289e95207117f129b
Administrator <zhaisa21@163.com>
2018-03-22 18:29:57 +0800

    test

--- src/test/java/com/irongbeitest/CreateZhiTouTest.java
+++ src/test/java/com/irongbeitest/CreateZhiTouTest.java
@@ -7,7 +7,7 @@ public class CreateZhiTouTest {
 		CreateZhiTou cz = new CreateZhiTou();
 		for (int i = 1; i < 3; i++) {
 			try {
-				cz.createZhiTou("3", 3, 2018, 2, 7, i);// 周期、间隔、开始日期
+				cz.createZhiTou("3", 3, 2018, 2, 22, i);// 周期、间隔、开始日期
 
 			} catch (Exception e) {
 				e.printStackTrace();
